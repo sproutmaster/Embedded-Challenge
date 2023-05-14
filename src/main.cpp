8801781c8824885909803303b7a7e241edbee3cc
benlam12 <57205951+benlam12@users.noreply.github.com>
2023-05-14 17:04:09 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,31 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_Circuit_Playground.h>
-
+int LED = 0;
 void setup() {
 // setup button to record/stop gesture recognition
 // setup neo pixels to turn green when unlocked and red for locked
 // play a beep when unlocked
-
+  DDRC |= (1<<7); //C7 is the LED PIN
+  DDRF &= ~(1<<6); //F6 is the right push button
+  DDRD &= ~(1<<4); //D4 is the left push button
+}
 
 }
 
-void loop() {
+void loop() 
+{
+  if((PINF & (1<<6)) != 0)
+  {
+    PORTC |= (1<<7);
+    LED = 1;
+  }
 
+  if((PIND & (1<<4)) != 0)
+  {
+    PORTC &= ~(1<<7);
+    LED = 0;
+  }
 // when button is pressed to record mode, read accelerometer value x, y and z till stop button is pressed. Take the average of magnitude of each of those
 // and store it. Compare it when the stop button is pressed. If it matches, turn neopixels green and beep, else red. Repeat
 
